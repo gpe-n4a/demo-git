f35c5c1d63a4e542cac8a32b5b391f0952c7e057
Guillaume Petitpierre <gpetitpierre@net4all.ch>
2017-12-27 11:31:44 +0100

    Added feature: Find the number

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#define GOAL_NBR 37
 
 int main() {
-	printf("Hello, world !\n");
+	int input = 0;
+	int tries = 0;
+
+	while (input != GOAL_NBR){
+		printf("Enter a guess: ");
+		scanf("%d", &input);
+
+		if(input < GOAL_NBR){
+			printf("Too low\n");
+		} else if(input > GOAL_NBR){
+			printf("Too high\n");
+		}
+		tries++;
+	}
+	printf("Well done ! You've found the number in %d tries !", tries);
 	return 0;
 }
